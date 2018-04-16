ac6af3ef1721bda412e2db84bc8a60a203f6594c
Jake Browning <jake.browning@emu-analytics.com>
2018-04-16 10:49:53 +0100

    Rm events demo

--- demos/demo_events.py
+++
@@ -1,8 +0,0 @@
-from booksoup import BookSoup
-
-me = BookSoup("facebook-data")
-
-events = me.load_all_events()
-
-for event in events.attending:
-    print(event.title, event.description, event.location, event.timestamp, event.latlon)

9266a5e698d654d8d493a729448c4a9d1c6fc0cd
Jake Browning <jake.browning@emu-analytics.com>
2018-04-17 11:08:24 +0100

    Remove test

--- t.py
+++
@@ -1,9 +0,0 @@
-from booksoup import BookSoup
-
-me = BookSoup("facebook-data")
-
-# Get a conversation by name
-convo = me.load_conversation("Jaki Reid")
-
-# Print participants of the conversation
-print(convo.participants)
